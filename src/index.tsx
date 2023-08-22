da6248ea6bcf455836bb97ccd6250e450f21c1d6
Aleksandr_UB <bogdanov777000@mail.ru>
2023-08-22 16:53:46 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,9 @@ import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-    // для GitHubPages необходимо использовать HashRouter
-    // <HashRouter></HashRouter>
     <BrowserRouter>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
     </BrowserRouter>, document.getElementById('root')
 );
-
-//rerenderEntireTree();
-
-// store.subscribe( () => {
-//     rerenderEntireTree()
-// });
